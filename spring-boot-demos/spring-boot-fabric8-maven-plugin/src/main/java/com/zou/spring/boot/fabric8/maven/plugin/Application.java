781c8b2f95ac6dcd8a77e8f2c2bfad00839f7c59
weihu <weihubeats@163.com>
2022-11-16 17:39:07 +0800

    添加fabric8 maven 插件

---
+++ spring-boot-demos/spring-boot-fabric8-maven-plugin/src/main/java/com/zou/spring/boot/fabric8/maven/plugin/Application.java
@@ -0,0 +1,17 @@
+package com.zou.spring.boot.fabric8.maven.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@author : wh
+ *@date : 2022/11/11 18:09
+ *@description:
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
