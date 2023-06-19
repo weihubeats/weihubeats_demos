97fef5577df5588575d7b722dad03cce1d881662
weihu <weihu@apache.org>
2023-06-19 19:23:01 +0800

    add SigNoz

--- spring-boot-demos/spring-boot-signoz/src/main/java/com/order/OrderApplication.java
+++ spring-boot-demos/spring-boot-signoz/src/main/java/com/order/OrderApplication.java
@@ -1,4 +1,4 @@
-package order;
+package com.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-demos/spring-boot-signoz/src/main/java/com/order/controller/OrderController.java
+++ spring-boot-demos/spring-boot-signoz/src/main/java/com/order/controller/OrderController.java
@@ -1,4 +1,4 @@
-package order.controller;
+package com.order.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
