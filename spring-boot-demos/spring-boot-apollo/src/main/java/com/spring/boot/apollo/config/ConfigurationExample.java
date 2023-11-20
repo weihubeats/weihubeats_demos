a861c31eae8be5acb25cb8cb6094b8422d30de65
weihu <weihu@apache.org>
2023-11-20 10:39:10 +0800

    add apollo demo

--- spring-boot-demos/spring-boot-apollo/src/main/java/com/spring/boot/apollo/config/ConfigurationExample.java
+++ spring-boot-demos/spring-boot-apollo/src/main/java/com/spring/boot/apollo/config/ConfigurationExample.java
@@ -2,8 +2,11 @@ package com.spring.boot.apollo.config;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * @author : wh
@@ -11,15 +14,17 @@ import org.springframework.context.annotation.Configuration;
  * @description: 各种配置Java Bean https://blog.csdn.net/qq_42651904/article/details/126947911
  */
 @Configuration(proxyBeanMethods = false)
+@Component
+@Getter
 public class ConfigurationExample {
 
     /**
      * 配置文件 test1,xiaozou,test2
-     */
+     *//*
     @Value("${test.list}")
     private List<String> list;
 
-    /**
+    *//**
      * 配置文件 
      * {
      *     "test": "vlaue1,vlaue2",
@@ -28,6 +33,18 @@ public class ConfigurationExample {
      */
     @Value("#{${test.map}}")
     private Map<String, List<String>> testMap;
+
+    /**
+     * {"1,2": "vlaue1,vlaue2", "3,4": "xiaozouValue,xiaozouValue1"}
+     */
+    @Value("#{${test.list.map}}")
+    private Map<Set<Integer>, Set<String>> setSetMap;
+
+    /**
+     * {"1,2": "vlaue1,vlaue2", "3,4": "xiaozouValue,xiaozouValue1"}
+     */
+    @Value("#{${test.list.map}}")
+    private Map<List<Integer>, List<String>> listListMap;
 
 
 
--- spring-boot-demos/spring-boot-apollo/src/main/java/com/spring/boot/apollo/controller/TestController.java
+++ spring-boot-demos/spring-boot-apollo/src/main/java/com/spring/boot/apollo/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.spring.boot.apollo.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.spring.boot.apollo.config.ConfigurationExample;
 import com.spring.boot.apollo.config.TestProperties;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	private final TestProperties testProperties;
+
+	private final ConfigurationExample configurationExample;
 	
 	@GetMapping("/test")
 	public void test() {
 		System.out.println(testProperties);
 		
+	}
+	
+	@GetMapping("/test1")
+	public String test1() {
+		System.out.println(JSON.toJSONString(configurationExample));
+		return JSON.toJSONString(configurationExample.getListListMap());
 	}
 }
