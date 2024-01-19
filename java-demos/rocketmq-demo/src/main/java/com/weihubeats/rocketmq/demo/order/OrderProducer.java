f36840c35b100fcde8f791b95089f3e96e1207a6
weihu <weihu@apache.org>
2024-01-19 19:39:40 +0800

    format code

--- java-demos/rocketmq-demo/src/main/java/com/weihubeats/rocketmq/demo/order/OrderProducer.java
+++ java-demos/rocketmq-demo/src/main/java/com/weihubeats/rocketmq/demo/order/OrderProducer.java
@@ -4,12 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.common.message.MessageQueue;
 
 /**
  *@author : wh
@@ -20,16 +17,11 @@ public class OrderProducer {
 
 	public static void main(String[] args) throws Exception {
 		DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
-
 		producer.setNamesrvAddr("127.0.0.1:9876");
-
 		producer.start();
-
 		String[] tags = new String[]{"TagA", "TagC", "TagD"};
-
 		// 订单列表
 		List<OrderStep> orderList = new OrderProducer().buildOrders();
-
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String dateStr = sdf.format(date);
@@ -37,22 +29,16 @@ public class OrderProducer {
 			// 加个时间前缀
 			String body = dateStr + " Hello RocketMQ " + orderList.get(i);
 			Message msg = new Message("TopicTest", tags[i % tags.length], "KEY" + i, body.getBytes());
-
-			SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
-				@Override
-				public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
-					Long id = (Long) arg;  //根据订单id选择发送queue
-					long index = id % mqs.size();
-					return mqs.get((int) index);
-				}
-			}, orderList.get(i).getOrderId());//订单id
-
+			SendResult sendResult = producer.send(msg, (mqs, msg1, arg) -> {
+                Long id = (Long) arg;  //根据订单id选择发送queue
+                long index = id % mqs.size();
+                return mqs.get((int) index);
+            }, orderList.get(i).getOrderId());//订单id
 			System.out.println(String.format("SendResult status:%s, queueId:%d, body:%s",
 					sendResult.getSendStatus(),
 					sendResult.getMessageQueue().getQueueId(),
 					body));
 		}
-
 		producer.shutdown();
 	}
 
